c196afe8a9f3aa72591a59880c10a6193838014b
Jared Morgan <jmorgan@morgancreative.net>
2009-06-16 10:32:57 -0500

    Add Paginate::DM#paginate method.
    Loosely based on various pagination code I've used or written before, particularly http://github.com/lholden/dm-is-paginated .

--- lib/paginate/dm.rb
+++ lib/paginate/dm.rb
@@ -1,5 +1,72 @@
 module Paginate
+  
+  # .pagination method for DataMapper models.  
   module DM
+    DEFAULT_LIMIT = 100
     
+    # Implementation of .paginate for DataMapper.
+    # (This is loosely based on http://github.com/lholden/dm-is-paginated)
+    #
+    # To make available to your model:
+    # class MyModel
+    #   include DataMapper::Resource
+    #   extend Paginate::DM
+    #   ...
+    # end
+    # 
+    # Accepts same options as DataMapper::Resource.all, plus:
+    # - +page+: The desired page number, 1-indexed. Negative numbers represent
+    #   page from the last page (with -1) being the last.
+    #
+    # The +limit+ option should also be specified and represents records per
+    # page. Any +offset+ option will be overriden.
+    #
+    # Returns the records for the given page number, with other options 
+    # processed by +all+. In addition, two methods are added to the result:
+    # - +pages+: The total number of pages that the given options would produce.
+    # - +current_page+: The number of the current page (as actually returned).
+    #   This is never a negative number even if given.
+    # If the +page+ option is zero, or less than (-1 * pages) or greater than 
+    # the total pages, it will be recalculated to return either page 1 or the 
+    # last page, respectively.
+    #
+    # This is not strictly for DataMapper. The class should respond to .all and
+    # .count with options similar to DataMapper (offset and limit are a must)
+    # For example, this would probably work in an ActiveRecord model.
+    def paginate(options = {})
+      page = options.delete(:page).to_i
+      limit = options[:limit] ? options[:limit].to_i : DEFAULT_LIMIT
+      order = options[:order]
+      
+      # Remove some options before calling +count+ that are not applicable.
+      # order and limit are needed later and have been saved above.
+      [:offset, :limit, :order].each do |key|
+        options.delete(key)
+      end
+      
+      # Determine total number of pages and set offset option.
+      pages = (self.count(options).to_f / limit).ceil
+      page = (pages + 1 + page) if page < 0 # Negative page
+      page = pages if page > pages # page should not be more than total pages
+      page = 1 if page < 1 # Minimum is 1 even if 0 records.
+      pages = 1 if pages < 1 # Minimum is 1 even if 0 records.
+      options[:offset] = ((page - 1) * limit)
+      
+      # Add limit and order back into options, from above.
+      options[:limit] = limit
+      options[:order] = order if order
+      
+      # Call +all+.
+      collection = all(options)
+
+      # Create +pages+ and +current_page+ methods for collection, for use by
+      # pagination links.
+      collection.instance_variable_set(:@pages, pages)
+      collection.instance_variable_set(:@current_page, page)
+      def collection.pages; @pages; end
+      def collection.current_page; @current_page; end
+      
+      return collection
+    end
   end
 end
--- spec/paginate/dm_spec.rb
+++ spec/paginate/dm_spec.rb
@@ -1,44 +1,168 @@
 require 'dm-core'
+require 'dm-aggregates'
 require File.dirname(__FILE__) + '/../spec_helper'
 require File.dirname(__FILE__) + '/../fixtures/dm'
 
 describe Paginate::DM do
+  Model = Paginate::Fixtures::DmModel
   before(:all) do
-    Paginate::Fixtures::DmModel.auto_migrate!
+    Model.auto_migrate!
   end
   
   before(:each) do
-    Paginate::Fixtures::DmModel.all.destroy!
-    1.upto(200) do |i|
-      Paginate::Fixtures::DmModel.create(:name => 'Person #{i}')
+    Model.all.destroy!
+    1.upto(50) do |i|
+      Model.create(:name => "Person #{i}")
     end
     @limit = 10
   end
   
   describe '#paginate' do
-    it 'should return correct results per page (correct offset, limit)'
-    it 'should add #pages method to collection'
-    it 'should return page 1 if options[:page] == 0'
-    it 'should return last page if options[:page] > total pages'
-    it 'should return last page for page == -1'
-    it 'should return page from last for negative options[:page]'
-    it 'should return first page for very negative options[:page] values'
-    it 'should order per options passed'
-    it 'should accept conditions (representative of options to .all)'
-    it 'should set limit by options[:limit]'
-    it 'should default limit to DEFAULT_LIMIT'
-    it 'should have at least one result on the last page'
+    it "should set limit and offset options" do
+      Model.should_receive(:all).with(:offset => 0, :limit => @limit)
+      Model.paginate(:page => 1, :limit => @limit)
+      
+      Model.should_receive(:all).with(:offset => 0, :limit => @limit + 1)
+      Model.paginate(:page => 1, :limit => @limit + 1)
+      
+      Model.should_receive(:all).with(:offset => 0, :limit => @limit + 2)
+      Model.paginate(:limit => @limit + 2)
+      
+      Model.should_receive(:all).with(:offset => @limit, :limit => @limit)
+      Model.paginate(:page => 2, :limit => @limit)
+
+      Model.should_receive(:all).with(:offset => 40, :limit => @limit)
+      Model.paginate(:page => -1, :limit => @limit)
+    end
+    
+    it 'should return correct results per page (correct offset, limit)' do
+      Model.paginate(:page => 1, :limit => 5).collect {|r| r.name}.should ==
+        ["Person 1", "Person 2", "Person 3", "Person 4", "Person 5"]      
+            
+      Model.paginate(:page => 2, :limit => 5).collect {|r| r.name}.should ==
+        ["Person 6", "Person 7", "Person 8", "Person 9", "Person 10"]
+    end
+    
+    it 'should return page 1 if options[:page] == 0' do
+      Model.paginate(:page => 0, :limit => 3).collect {|r| r.name}.should ==
+        ["Person 1", "Person 2", "Person 3"]
+    end
+    
+    it 'should return page 1 if options[:page] not set' do
+      Model.paginate(:limit => 3, :page => -50).collect {|r| r.name}.should ==
+        ["Person 1", "Person 2", "Person 3"]
+    end
+    
+    it 'should return last page if options[:page] > total pages' do
+      Model.paginate(:limit => 5, :page => 11).collect {|r| r.name}.should ==
+        ["Person 46", "Person 47", "Person 48", "Person 49", "Person 50"]
+
+      Model.paginate(:limit => 5, :page => 20).collect {|r| r.name}.should ==
+        ["Person 46", "Person 47", "Person 48", "Person 49", "Person 50"]
+    end
+    
+    it 'should return last page for page == -1' do
+      Model.paginate(:limit => 5, :page => -1).collect {|r| r.name}.should ==
+        ["Person 46", "Person 47", "Person 48", "Person 49", "Person 50"]
+        
+      Model.paginate(:limit => 3, :page => -1).collect {|r| r.name}.should ==
+        ["Person 49", "Person 50"]
+    end
+    
+    it 'should return page from last for negative options[:page]' do
+      Model.paginate(:limit => 3, :page => -2).collect {|r| r.name}.should ==
+        ["Person 46", "Person 47", "Person 48"]
+      
+      Model.paginate(:limit => 3, :page => -3).collect {|r| r.name}.should ==
+        ["Person 43", "Person 44", "Person 45"]
+    end
+    
+    it 'should return first page for very negative options[:page] values' do
+      Model.paginate(:limit => 3, :page => -50).collect {|r| r.name}.should ==
+        ["Person 1", "Person 2", "Person 3"]
+    end
+    
+    it 'should order per options passed' do
+      Model.paginate(:limit => 3, :page => 1, :order => [:id.desc]).collect {|r| r.name}.should ==
+        ["Person 50", "Person 49", "Person 48"]
+    end
+    
+    it 'should accept conditions (representative of options to .all)' do
+      collection = Model.paginate(:limit => 3, :page => 1, :name.like => 'Person 3%')
+      collection.collect {|r| r.name}.should ==
+        ["Person 3", "Person 30", "Person 31"]
+      collection.pages.should == 4
+    end
+    
+    it 'should default limit to DEFAULT_LIMIT' do
+      Model.should_receive(:all).with(:offset => 0, :limit => Paginate::DM::DEFAULT_LIMIT)
+      Model.paginate
+    end
+    
+    it 'should have at least one result on the last page' do
+      Model.paginate(:limit => 5, :page => 10).should_not be_empty
+      
+      collection = Model.paginate(:limit => 5, :page => 11)
+      collection.should_not be_empty
+      collection.current_page.should == 10
+    end
   end
   
   describe '#current_page' do
-    it 'should be added to collection'
-    it 'should be 1 if there are no results'
-    it 'should be >= 1 and <= #pages (ie actual page, not given page)'
+    it 'should be added to collection' do
+      collection = Model.paginate(:limit => 10)
+      # collection.methods.should include("current_page")
+      # Not sure why the above won't work. #singleton_methods doesn't either.
+      collection.current_page.should be_kind_of(Integer)
+    end
+    
+    it 'should be the number of the current page' do
+      collection = Model.paginate(:limit => 10, :page => 2)
+      collection.current_page.should == 2
+
+      collection = Model.paginate(:limit => 10, :page => 5)
+      collection.current_page.should == 5
+    end
+    
+    it 'should be 1 if there are no results' do
+      Model.all.destroy!
+      collection = Model.paginate(:limit => 10)
+      collection.current_page.should == 1
+    end
+    
+    it 'should be >= 1 and <= #pages (ie actual page, not given page)' do
+      collection = Model.paginate(:limit => 10, :page => -100)
+      collection.current_page.should == 1
+      collection = Model.paginate(:limit => 10, :page => 100)
+      collection.current_page.should == 5
+      collection = Model.paginate(:limit => 10, :page => -1)
+      collection.current_page.should == 5
+      collection = Model.paginate(:limit => 10, :page => 0)
+      collection.current_page.should == 1
+    end
   end
   
   describe '#pages' do
-    it 'should be the total number of pages for the collection'
-    it 'should be added to collection'
-    it 'should be 1 if there are no results'
+    it 'should be added to collection' do
+      collection = Model.paginate(:limit => 10)
+      # collection.methods.should include("pages")
+      # Not sure why the above won't work. #singleton_methods doesn't either.
+      collection.pages.should be_kind_of(Integer)
+    end
+    
+    it 'should be the total number of pages for the collection' do
+      collection = Model.paginate(:limit => 10)
+      collection.pages.should == 5
+      collection = Model.paginate(:limit => 49)
+      collection.pages.should == 2
+      collection = Model.paginate(:limit => 50)
+      collection.pages.should == 1
+    end
+    
+    it 'should be 1 if there are no results' do
+      Model.all.destroy!
+      collection = Model.paginate(:limit => 10)
+      collection.pages.should == 1
+    end
   end
 end
